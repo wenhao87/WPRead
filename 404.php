ee2b3295448cfe8bedd1b80008a3f4f16be5815f
wenhao87 <vahichen@gmail.com>
2016-01-27 10:06:55 -0700

    soft tabs with 2 spaces

--- 404.php
+++ 404.php
@@ -1,24 +1,19 @@
 <?php
-	/* 404 Page Template */
+  /* 404 Page Template */
 ?>
 
-<?php
-	get_header();
-?>
+<?php get_header(); ?>
 
 <div id="primary" class="site-content">
-	<div id="content" role="main">
-		<div class="not-found">
-			<h1><?php echo __( '404', 'read' ); ?></h1>
-			
-			<p><?php echo __( 'The page you are looking for was not found!', 'read' ); ?></p>
-		</div>
-		<!-- end .not-found -->
-	</div>
-	<!-- end #content -->
+  <div id="content" role="main">
+    <div class="not-found">
+      <h1><?php echo __( '404', 'read' ); ?></h1>
+      <p><?php echo __( 'The page you are looking for was not found!', 'read' ); ?></p>
+    </div>
+    <!-- end .not-found -->
+  </div>
+  <!-- end #content -->
 </div>
 <!-- end #primary -->
 
-<?php
-	get_footer();
-?>
+<?php get_footer(); ?>
